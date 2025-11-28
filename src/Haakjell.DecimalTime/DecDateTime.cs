eacc4ac0f8874abfe927eb4cbce48c4d5f87078a
Håkon Anders Strømsodd <haakon8855@gmail.com>
2025-11-28 10:55:20 +0100

    Add decimal datetime project

---
+++ src/Haakjell.DecimalTime/DecDateTime.cs
@@ -0,0 +1,39 @@
+﻿namespace Haakjell.DecimalTime;
+
+public sealed class DecDateTime
+{
+    public DateTime Value { get; private init; }
+
+    public static DecDateTime FromDateTime(DateTime dateTime) => new() { Value = dateTime };
+
+    private static bool EqualOperator(DecDateTime? left, DecDateTime? right)
+    {
+        if (left is null)
+            return right is null;
+        return left.Equals(right);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null || obj.GetType() != GetType())
+            return false;
+        var other = (DecDateTime)obj;
+        return GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+    }
+
+    public static bool operator ==(DecDateTime left, DecDateTime right) => EqualOperator(left, right);
+
+    public static bool operator !=(DecDateTime left, DecDateTime right) => !EqualOperator(left, right);
+
+    public override int GetHashCode()
+    {
+        return GetEqualityComponents()
+            .Select(x => x.GetHashCode())
+            .Aggregate((x, y) => x ^ y);
+    }
+
+    private IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
